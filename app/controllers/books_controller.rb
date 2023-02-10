5546fbf984525a82567e5dcf1445b0a2aa987b6e
hashihana <120104605+hashihana@users.noreply.github.com>
2023-02-10 14:10:41 +0000

    [Finish] Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,6 +27,8 @@ class BooksController < ApplicationController
 
   
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
       flash[:notice]="You have creatad book successfully."
       redirect_to book_path(@book)
